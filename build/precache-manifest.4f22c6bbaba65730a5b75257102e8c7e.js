1106414916e17d142bd8685b102ea106c8443497
HamdIslam <hamdislam@gmail.com>
2020-07-08 14:34:39 +0400

    Added Build

---
+++ build/precache-manifest.4f22c6bbaba65730a5b75257102e8c7e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "a95e3859b022a9c51ce7",
+    "url": "/static/css/main.f460f374.chunk.css"
+  },
+  {
+    "revision": "a95e3859b022a9c51ce7",
+    "url": "/static/js/main.a95e3859.chunk.js"
+  },
+  {
+    "revision": "bce4a348783ab74d34f9",
+    "url": "/static/css/1.4b41f0f0.chunk.css"
+  },
+  {
+    "revision": "bce4a348783ab74d34f9",
+    "url": "/static/js/1.bce4a348.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "016c14adad95a0e21a2ae4fbc36955c6",
+    "url": "/static/media/MaterialIcons-Regular.016c14ad.eot"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "/static/media/MaterialIcons-Regular.8a9a261c.woff2"
+  },
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "/static/media/MaterialIcons-Regular.c38ebd3c.woff"
+  },
+  {
+    "revision": "d41ae58dab60e355ae3872ebf2f1403a",
+    "url": "/static/media/MaterialIcons-Regular.d41ae58d.ttf"
+  },
+  {
+    "revision": "bdaf62633acc3d8e43a84e2d3d9b7ca7",
+    "url": "/index.html"
+  }
+];
